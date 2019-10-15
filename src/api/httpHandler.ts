451ba52c08251930bf2acc3efa2f33ded3b92038
Marcos Gutierrez <gmarcos87@gmail.com>
2019-10-15 09:57:41 -0300

    cahnge api to httpHandler file

---
+++ src/api/httpHandler.ts
@@ -0,0 +1,88 @@
+import EventSource from './events';
+import Base64 from '../helpers/base64';
+import uuid from 'uuid/v4';
+import { endpoints } from './endpoints';
+import { basicResponse, apiRequest, apiState} from './types'
+
+let requestList = new Map();
+
+const logRequest = (type: string, data: any) => {
+    //Only for debug
+    console.log('API ', type, data)
+}
+
+const pullRequest = ()  => {
+    if(requestList.size > 0) {
+        requestList.values().next().value.callRequest()
+    }
+}
+
+const apiHttp = (
+        url: string,
+        path: string,
+        data: object,
+        method: string,
+        headers: Headers
+    ) => new Promise<basicResponse>((res, rej) => { 
+        const id = uuid();
+        logRequest('NEW', id)
+        requestList.set(id, { callRequest : () => {
+            logRequest('CALLING', {path, data, method, headers})
+            fetch(`${url}${path}`, { headers, method, body: data? JSON.stringify(data): undefined})
+                .then(result => { 
+                    requestList.delete(id)
+                    return  result.json()
+                })
+                .then((data) => {
+                    res(data)
+                    logRequest('RESULT', {id, data})
+                    pullRequest()
+                })
+                .catch(e => {
+                    rej(e)
+                    requestList.delete(id)
+                    pullRequest()
+                })
+        }
+    })
+
+    if(requestList.size === 1) {
+        requestList.values().next().value.callRequest()
+    }
+})
+
+const api = {
+    state: {
+        url:'http://localhost',
+        port: '9092',
+        login: false,
+        user: {
+            mLocationId: ''
+        },
+        password: ''
+    },
+    isLogged: () => api.state.login,
+    setState: (state: apiState) => api.state = api.state = {...api.state, ...state},
+    getUrl: () => api.state.url+':'+api.state.port,
+    getBtoa: () => 'Basic ' + Base64.btoa(api.state.user.mLocationId + ":" + api.state.password),
+    send:({ path, data, method }:apiRequest) =>  {
+        let headers = new Headers()
+        //Add auth headers
+        if (api.isLogged()) {
+            headers.set('Authorization', api.getBtoa())
+        }
+        return apiHttp(api.getUrl(), path, data, method || 'POST', headers)
+    },
+    sendEvent: ({ path, data }:apiRequest) => {
+        const evtSource = new EventSource(api.getUrl()+path, {
+            data,
+            headers: {
+                'Authorization': api.getBtoa()
+            }
+        });
+        return Promise.resolve(evtSource);
+    },
+    endpoints: () => endpoints(api.send)
+};
+
+export default api;

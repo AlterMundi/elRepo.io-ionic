a85b768f379a2ba20836e2e4ec6dccf1649dc57b
Marcos Gutierrez <gmarcos87@gmail.com>
2019-10-16 10:58:51 -0300

    change api state after login

--- src/api/actions/user/login.ts
+++ src/api/actions/user/login.ts
@@ -4,6 +4,15 @@ import { credentials } from "../../../helpers/localStorage"
 export const login = async({account, password}: credentials):Promise<string> => {
     const { attemptLogin } = api.endpoints().rsLoginHelper
     const { retval, errorMessge } = await attemptLogin({ account, password })
-    if (retval) return account
+    if (retval) {
+        api.setState({
+            user:{
+                mLocationId:account
+            },
+            password,
+            login: true,
+        })
+        return account
+    }
     throw(new Error(errorMessge))
 }

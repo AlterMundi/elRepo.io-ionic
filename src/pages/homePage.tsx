ea82f1cbd053dce0243be53c28d91acb5af16c1e
Marcos Gutierrez <gmarcos87@gmail.com>
2019-10-17 21:14:23 -0300

    add logo in home screen

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import {
-  IonContent, IonPage
+  IonContent, IonPage, IonImg
 } from '@ionic/react';
 
 import './homePage.css'
+import logo from './logo';
 
 const HomePage: React.FC = () => {
   return (
     <IonPage>
-      <IonContent class="ion-padding-start ion-padding-end" fullscreen color={'primary'}>
+      <IonContent class="ion-padding-start ion-padding-end homePage" fullscreen>
         <div className={'search-area'}>
+          <IonImg src={logo} className={'homeLogo'} />
           <div className={'search-input search-input--large'} >
             <input type='text' placeholder={'Search content...'}/>
           </div>

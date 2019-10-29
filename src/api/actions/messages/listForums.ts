a8c07987169212125d91090e30a13acf075209a2
Marcos Gutierrez <gmarcos87@gmail.com>
2019-10-29 08:58:48 -0300

    list messages and forum

---
+++ src/api/actions/messages/listForums.ts
@@ -0,0 +1,49 @@
+import api from "../../httpHandler"
+import { ForumRS } from "../../types";
+
+export interface Forum {
+    name: string,
+    id: string,
+    authorId: string,
+    messagesCount: number,
+    lastPublish: number
+}
+
+const filterAndFormatForums = (forums:ForumRS[]):Forum[] =>
+    forums
+        //Only repo forums
+        .filter(forum => forum.mGroupName.includes('repo_'))
+        //Only subscrived ones
+        .filter(forum => forum.mSubscribeFlags === 7)
+        //Format response
+        .map(forum => ({
+            name: forum.mGroupName.split('repo_')[1],
+            id: forum.mGroupId,
+            authorId: forum.mAuthorId,
+            messagesCount: forum.mVisibleMsgCount,
+            lastPublish: forum.mLastPost
+        }));
+
+export const listForums = async() => {
+    const { getForumsSummaries } = api.endpoints().rsGxsForums
+    
+    const { forums, retval, errorMessage } = await getForumsSummaries()
+    if (!retval) {
+        throw(new Error(errorMessage))
+    }
+
+    return filterAndFormatForums(forums)
+}
+
+export const listPosts = async(forumId: string) => {
+    if (!forumId) throw new Error('forumId is required');
+
+    const { getForumMsgMetaData } = api.endpoints().rsGxsForums
+    const {msgMetas} = await getForumMsgMetaData({forumId})
+    
+    return msgMetas.map(msg => ({
+        title: msg.mMsgName,
+        author: msg.mAuthorId,
+        date: msg.mPublishTs
+    }))
+}
